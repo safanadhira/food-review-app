f9284793986ec188a1ff3d974fab538f340ceab6
safa <safanadhira47@gmail.com>
2025-09-16 23:54:35 +0700

    tutorial 11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PBKK TEST</title>
-</head>
-<body>
-    <h1>Welcome to PBKK</h1>
-    <p>Try to click this button below</p>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PBKK PROJECT</title>
 
-    <a href="/ninjas" class="btn">
-        Find Ninjas!
+  @vite('resources/css/app.css')
+</head>
+<body class="flex flex-col items-center justify-center min-h-screen text-center">
+  <h1>Welcome to My Website!</h1>
+  <p>I have some Ninjas! Click the button below.</p>
+  
+  <a href="/ninjas" class="btn mt-4 inline-block">
+    Find Ninjas!
+  </a>
 </body>
 </html>
